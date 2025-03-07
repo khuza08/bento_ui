11c52b253d6334c82a2ca0bc1e6d7b1102a7e4d3
huza <huzaifahasif3@gmail.com>
2025-03-07 23:41:42 +0700

    update audio function

--- assets/js/jasjus.js
+++ assets/js/jasjus.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const audio = new Audio("assets/src/music/1.mp3");
-    audio.volume = 0.6
+    audio.volume = 0.5; 
+    
     let hasPlayed = false;
 
-    document.querySelectorAll(".bento").forEach((bento) => {
-        bento.addEventListener("mouseenter", () => {
-            if (!hasPlayed) {
-                audio.play();
-                hasPlayed = true;
-            }
-        });
+    document.querySelector(".nama").addEventListener("mouseenter", () => {
+        if (!hasPlayed) {
+            audio.play().catch(() => {
+                console.log();
+            });
+            hasPlayed = true;
+        }
     });
 
     audio.addEventListener("ended", () => {
-        hasPlayed = false;
-    });
+        hasPlayed = false; 
 });
